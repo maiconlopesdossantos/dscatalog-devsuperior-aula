784f87c99786d8958904a7cd9559aa688004e6dd
maicon.lopes <maicon.lopes@agi.com.br>
2022-07-26 09:12:25 -0300

    post

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -31,4 +31,11 @@ public class CategoryService {
         Category entity = obj.orElseThrow(() -> new EntityNotFoundException("ENTITY NOT FOUND"));
         return new CategoryDTO(entity);
     }
+    @Transactional
+    public CategoryDTO insert(CategoryDTO dto) {
+        Category entity = new Category();
+        entity.setName(dto.getName());
+        entity = repository.save(entity);
+        return new CategoryDTO(entity);
+    }
 }
